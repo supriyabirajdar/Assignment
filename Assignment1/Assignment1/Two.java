6da278afeb67fe069263760402359b18ce2908dc
supriyabirajdar <47962411+supriyabirajdar@users.noreply.github.com>
2019-02-26 14:46:10 +0530

    Delete Two.java

--- Assignment1/Assignment1/Two.java
+++
@@ -1,15 +0,0 @@
-/* Program to print 5 times “Marvellous” on screen. */
-
-public class Two{
-
-    public static void main(String[] args) {
-
-        int i=1;
-
-       
-        for(int i = 1; i <=5; ++i)
-          {
-             System.out.print("Marvellous\n ");
-        }
-    }
-}
